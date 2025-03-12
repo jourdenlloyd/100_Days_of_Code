e505c7047971062b265a9c2ddd2eea18db4f60f4
jourdenlloyd <jourdenlloyd@icloud.com>
2025-03-12 14:32:45 -0400

    Finished most of Day 4

--- Day 4/Random Module/task.py
+++ Day 4/Random Module/task.py
@@ -1,3 +1,15 @@
 import random
-rand_num = random.randint(1,10)
-print(rand_num)
+
+# rand_num = random.randint(1, 10)
+# print(rand_num)
+# from 1 to 9 because 10 isn't included
+
+# rand_num = random.random() * 10
+# print(rand_num)
+# from to get float numbers
+
+
+random_coin_face = random.randint(0,1)
+if random_coin_face == 0:
+    print("Heads")
+else: print("Tails")
