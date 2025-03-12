aac8fb98d79d34b787fcee14db5eb7c48a717925
jourdenlloyd <jourdenlloyd@icloud.com>
2025-03-12 15:29:19 -0400

    Starting Day 5

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,32 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+#print(rock)
+
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("Rock[0], Paper[1], or Scissors[2]?\n"))
+# Note: it's worth checking if the user has made a valid choice before the next line of code.
+# If the user typed somthing other than 0, 1 or 2 the next line will give you an error.
+# You could for example write:
+if user_choice >= 0 and user_choice <= 2:
+    print("You chose:")
+    print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
+
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number. Try again!")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("Sorry, you lose!")
+elif computer_choice > user_choice:
+    print("Sorry, you lose!")
+elif user_choice > computer_choice:
+    print("You win!")
+elif computer_choice == user_choice:
+    print("It's a tie!")
